aee8824fc4cdcb7188394e34261ece25396ab875
Alexandru Tachici <alexandru.tachici@analog.com>
2021-10-12 14:59:11 +0300

    test: hmc7044: test priority selection
    Add a reference order list to clock dict and test if they
    are set ok.
    
    Signed-off-by: Alexandru Tachici <alexandru.tachici@analog.com>

--- test/hmc7044/test_hmc7044.py
+++ test/hmc7044/test_hmc7044.py
@@ -11,6 +11,7 @@ def test_hmc7044_add_nodes():
     d = dt.hmc7044_dt(dt_source="local_file", local_dt_filepath=dtb)
 
     clock = {
+        "reference_selection_order" : [0, 3, 2, 1],
         "n2": 24,
         "out_dividers": [3, 6, 384],
         "output_clocks": {
@@ -30,6 +31,21 @@ def test_hmc7044_add_nodes():
 
     # Check for updated DT
     assert node.get_property("adi,vcxo-frequency").value == config["vcxo"]
+
+    # Check input reference priorities
+    if ("reference_selection_order" in clock):
+        ref_order = []
+        priority = 0
+        ref_order_prop_val = node.get_property("adi,pll1-ref-prio-ctrl").value
+
+        # MSB (Fourth priority input [1:0]) .... (First priority input [1:0]) LSB
+        for ref_nr in clock["reference_selection_order"]:
+            if (ref_nr > 4):
+                raise Exception("Refernce number:" + str(ref_nr) + " invalid.")
+            ref_order.append((ref_order_prop_val >> (priority * 2)) & 0x3)
+            priority += 1
+
+        assert ref_order == clock["reference_selection_order"]
 
     divs = [clock['output_clocks'][oc]['divider']  for oc in clock['output_clocks']]
     for n in node.nodes:
