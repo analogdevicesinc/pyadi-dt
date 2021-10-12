1dd28ca811c7d1e1b752840b451f0ac93aeb1a70
Alexandru Tachici <alexandru.tachici@analog.com>
2021-10-12 22:10:51 +0300

    test: hcm7044: test additional output properties
    Signed-off-by: Alexandru Tachici <alexandru.tachici@analog.com>

--- test/hmc7044/test_hmc7044.py
+++ test/hmc7044/test_hmc7044.py
@@ -16,9 +16,17 @@ def test_hmc7044_add_nodes():
         "n2": 24,
         "out_dividers": [3, 6, 384],
         "output_clocks": {
-            "ADC": {"divider": 3, "rate": 1000000000.0},
-            "FPGA": {"divider": 6, "rate": 500000000.0},
-            "SYSREF": {"divider": 384, "rate": 7812500.0},
+            "ADC": {"divider": 3, "rate": 1000000000.0, "driver-mode": "CML",
+                "high-performance-mode-disable": True,
+                "startup-mode-dynamic-enable": True,
+                "dynamic-driver-enable": True,
+                "force-mute-enable": True,
+                "output-mux-mode": "CH_DIV",
+                "driver_impedances": "100_OHM"},
+            "FPGA": {"divider": 6, "rate": 500000000.0, "driver-mode": "CML",
+                "fine-delay": 16, "coarse-delay": 5},
+            "SYSREF": {"divider": 384, "rate": 7812500.0, "driver-mode": "CMOS",
+                "CMOS": {"P" : 0, "N" : 1}},
         },
         "r2": 2,
     }
@@ -76,6 +84,59 @@ def test_hmc7044_add_nodes():
             clock["output_clocks"].keys()
         )
         assert n.get_property("adi,divider").value in divs
-        assert n.get_property("adi,driver-mode").value == 2
+
+    # Test if all output clock nodes properties from config have been set in DT
+    for i in range(0, len(node.nodes)):
+        output_node = node.nodes[i]
+        output_node_name = output_node.get_property("adi,extended-name").value
+        assert output_node_name in clock["output_clocks"]
+
+        output_dict = clock["output_clocks"][output_node_name]
+
+        assert output_node.get_property("adi,divider").value == output_dict["divider"]
+
+        if "driver-mode" in output_dict:
+            driver_mode = d.driver_modes[output_dict["driver-mode"]]
+            prop = output_node.get_property("adi,driver-mode")
+            assert prop.value == driver_mode
+
+        if "high-performance-mode-disable" in output_dict:
+            prop = output_node.get_property("adi,high-performance-mode-disable")
+            assert prop != None
+
+        if "startup-mode-dynamic-enable" in output_dict:
+            prop = output_node.get_property("adi,startup-mode-dynamic-enable")
+            assert prop != None
+
+            if "dynamic-driver-enable" in output_dict:
+                prop = output_node.get_property("adi,dynamic-driver-enable")
+                assert prop != None
+
+            if "force-mute-enable" in output_dict:
+                prop = output_node.get_property("adi,force-mute-enable")
+                assert prop != None
+
+        if ("output-mux-mode" in output_dict):
+            mux_mode = d.output_mux_modes[output_dict["output-mux-mode"]]
+            prop = output_node.get_property("adi,output-mux-mode")
+            assert prop.value == mux_mode
+
+        if ("driver-impedance-mode" in output_dict):
+            impedance_mode = self.driver_impedances[output_dict["driver-impedance-mode"]]
+            prop = output_node.get_property("adi,driver-impedance-mode")
+            assert prop.value == impedance_mode
+
+        if "fine-delay" in output_dict:
+            fine_delay = output_node.get_property("adi,fine-analog-delay").value
+            assert fine_delay == output_dict["fine-delay"]
+
+        if "coarse-delay" in output_dict:
+            fine_delay = output_node.get_property("adi,coarse-digital-delay").value
+            assert fine_delay == output_dict["coarse-delay"]
+
+        if "CMOS" in output_dict:
+            impedance_prop_val = output_node.get_property("adi,driver-impedance-mode").value
+            impedance_prop_dict = output_dict["CMOS"]["P"] + (output_dict["CMOS"]["N"] << 1)
+            assert impedance_prop_val == impedance_prop_dict
 
     # d.write_out_dts("test.dts")
