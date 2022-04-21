analogdevicesinc/pyadi-dt

commit dea99849ef71d94567d791f3fdd800fdecdea8a9

hmc7044: add additional output properties
Allow user to edit the following output node properties:
- driver-mode
- high-performance-mode-disable
- startup-mode-dynamic-enable
- dynamic-driver-enable
    - force-mute-enable
    - output-mux-mode
- output-mux-mode
- driver-impedance-mode
- CMOS impedance mode (actually tells if  N output or P output works)
- fine-delay
- coarse-delay

Signed-off-by: Alexandru Tachici <alexandru.tachici@analog.com>

adidt/parts/hmc7044.py
@@ -8,13 +8,76 @@ from adidt.parts.clock_dt import clock_dt
 class hmc7044_dt(dt, clock_dt):
     """HMC7044 Device tree map class."""
 
+    pulse_gen_modes = {
+        "GEN_LEVEL_SENSITIVE" : 0,
+        "GEN_1_PULSE" : 1,
+        "GEN_2_PULSE" : 2,
+        "GEN_4_PULSE" : 3,
+        "GEN_8_PULSE" : 4,
+        "GEN_16_PULSE" : 5,
+        "GEN_CONT_PULSE" : 7,
+    }
+
+    driver_modes = {
+        "CML" : 0,
+        "LVPECL" : 1,
+        "LVDS" : 2,
+        "CMOS" : 3,
+    }
+
+    driver_impedances = {
+        "DISABLE" : 0,
+        "100_OHM" : 1,
+        "50_OHM" : 3,
+    }
+
+    output_mux_modes = {
+        "CH_DIV" : 0,
+        "ANALOG_DELAY" : 1,
+        "GROUP_PAIR" : 3,
+        "VCO_CLOCK" : 4,
+    }
+
     def set_clock_node(self, parent, clk, name, reg):
         node = fdt.Node(f"channel@{reg}")
 
         node.append(fdt.PropWords("reg", reg))
         node.append(fdt.PropStrings("adi,extended-name", str(name)))
         node.append(fdt.PropWords("adi,divider", clk["divider"]))
-        node.append(fdt.PropWords("adi,driver-mode", 2))
+
+        driver_mode = self.driver_modes[clk["driver-mode"]]
+        node.append(fdt.PropWords("adi,driver-mode", driver_mode))
+
+        if ("high-performance-mode-disable" in clk):
+            node.append(fdt.Property("adi,high-performance-mode-disable"))
+
+        if ("startup-mode-dynamic-enable" in clk):
+            node.append(fdt.Property("adi,startup-mode-dynamic-enable"))
+
+            if ("dynamic-driver-enable" in clk):
+                node.append(fdt.Property("adi,dynamic-driver-enable"))
+
+            if ("force-mute-enable" in clk):
+                node.append(fdt.Property("adi,force-mute-enable"))
+
+        if ("output-mux-mode" in clk):
+            mux_mode = self.output_mux_modes[clk["output-mux-mode"]]
+            node.append(fdt.PropWords("adi,output-mux-mode", mux_mode))
+
+        if ("driver-impedance-mode" in clk):
+            impedance_mode = self.driver_impedances[clk["driver-impedance-mode"]]
+            node.append(fdt.PropWords("adi,driver-impedance-mode", impedance_mode))
+
+        if ("fine-delay" in clk):
+            node.append(fdt.PropWords("adi,fine-analog-delay", clk["fine-delay"]))
+
+        if ("coarse-delay" in clk):
+            node.append(fdt.PropWords("adi,coarse-digital-delay", clk["coarse-delay"]))
+
+        # in CMOS mode, the impedance property describes the output status
+        if ("CMOS" in clk):
+            prop_val = clk["CMOS"]["P"] + (clk["CMOS"]["N"] << 1)
+            node.append(fdt.PropWords("adi,driver-impedance-mode", prop_val))
 
         parent.append(node)
 

commit 21abba9c64f0798d87b17821cef1cdb7ea2e1a38

hmc7044: fix CMOS settings
Position of bits responsible for enabling/disabling a CMOS channel
differs as function of channel register address. There is no
general rule. Add a map of all positions required for both P and N.

Fixes: dea99849ef71 ("hmc7044: add additional output properties")
Signed-off-by: Alexandru Tachici <alexandru.tachici@analog.com>

adidt/parts/hmc7044.py
@@ -38,6 +38,23 @@ class hmc7044_dt(dt, clock_dt):
         "VCO_CLOCK" : 4,
     }
 
+    cmos_outputs_reg_field_map = {
+        0 : {"P" : 1, "N" : 0},
+        1 : {"P" : 0, "N" : 1},
+        2 : {"P" : 0, "N" : 1},
+        3 : {"P" : 1, "N" : 0},
+        4 : {"P" : 0, "N" : 1},
+        5 : {"P" : 1, "N" : 0},
+        6 : {"P" : 1, "N" : 0},
+        7 : {"P" : 0, "N" : 1},
+        8 : {"P" : 0, "N" : 1},
+        9 : {"P" : 1, "N" : 0},
+        10 : {"P" : 1, "N" : 0},
+        11 : {"P" : 0, "N" : 1},
+        12 : {"P" : 0, "N" : 1},
+        13 : {"P" : 1, "N" : 0},
+    }
+
     def set_clock_node(self, parent, clk, name, reg):
         node = fdt.Node(f"channel@{reg}")
 
@@ -76,7 +93,8 @@ class hmc7044_dt(dt, clock_dt):
 
         # in CMOS mode, the impedance property describes the output status
         if ("CMOS" in clk):
-            prop_val = clk["CMOS"]["P"] + (clk["CMOS"]["N"] << 1)
+            prop_val = (clk["CMOS"]["P"] << self.cmos_outputs_reg_field_map[reg]["P"])
+            propval = prop_val | (clk["CMOS"]["N"] << self.cmos_outputs_reg_field_map[reg]["N"])
             node.append(fdt.PropWords("adi,driver-impedance-mode", prop_val))
 
         parent.append(node)