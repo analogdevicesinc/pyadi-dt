fa3a89e71c035572d49c565b87f7f549796c3fc6
Travis F. Collins <travis.collins@analog.com>
2022-07-11 18:14:39 -0600

    [WIP] ADRV9009 profile parsing and fragment generation
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     include_package_data=True,
     package_data={"adidt": ["templates/*.tmpl"]},
     py_modules=["adidt"],
-    install_requires=["Click", "fdt", "fabric", "bcrypt==3.2.2", "rich", "numpy"],
+    install_requires=["Click", "fdt", "fabric", "bcrypt==3.2.2", "rich", "numpy", "xmltodict"],
     entry_points={
         "console_scripts": [
             "adidtc = adidt.cli.main:cli",
