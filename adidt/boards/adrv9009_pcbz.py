eac0bbbcc095e81f735e3ff9b51290da07c52f0d
Liam Beguin <liambeguin@gmail.com>
2023-09-01 20:15:50 -0400

    boards: adrv9009_pcbz: integrate the ad9528 parser
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- adidt/boards/adrv9009_pcbz.py
+++ adidt/boards/adrv9009_pcbz.py
@@ -1,5 +1,5 @@
 from .layout import layout
-from ..parts.adrv9009 import parse_profile
+from ..parts import ad9528, adrv9009
 import numpy as np
 from pathlib import Path
 
@@ -23,13 +23,25 @@ class adrv9009_pcbz(layout):
         """
         if not filename.exists():
             raise Exception(f"Profile file not found: {filename}")
-        self.profile = parse_profile(filename)
+
+        self.xcvr_profile = adrv9009.parse_profile(filename)
+
+        ad9528_file = filename.parent / (filename.stem + '_AD9528.txt')
+        if not ad9528_file.exists():
+            raise Exception(f"AD9528 Profile file not found: {ad9528_file}")
+
+        self.clock_profile = ad9528.parse_profile(ad9528_file)
 
     def gen_dt_preprocess(self):
         return {
-            "rx": self.profile["rx"],
-            "tx": self.profile["tx"],
-            "orx": self.profile["orx"],
-            "lpbk": self.profile["lpbk"],
-            "clocks": self.profile["clocks"],
+            "pll1": self.clock_profile["pll1"],
+            "pll2": self.clock_profile["pll2"],
+            "sysref": self.clock_profile["sysref"],
+            "out": self.clock_profile["out"],
+
+            "rx": self.xcvr_profile["rx"],
+            "tx": self.xcvr_profile["tx"],
+            "orx": self.xcvr_profile["orx"],
+            "lpbk": self.xcvr_profile["lpbk"],
+            "clocks": self.xcvr_profile["clocks"],
         }
