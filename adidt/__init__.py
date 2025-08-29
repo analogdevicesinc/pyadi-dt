4add595696c5e5bc69c421da1d5a2b9b1089572f
Travis F. Collins <travis.collins@analog.com>
2025-08-29 09:18:02 -0600

    Add DT models and templates for AD9081+NSB
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- adidt/__init__.py
+++ adidt/__init__.py
@@ -7,5 +7,6 @@ from adidt.parts.adrv9009 import adrv9009_dt
 
 from adidt.boards.daq2 import daq2
 from adidt.boards.ad9081_fmc import ad9081_fmc
+from adidt.boards.ad9081_nv_sb_s10 import ad9081_nv_sb_s10
 from adidt.boards.adrv9009_zu11eg import adrv9009_zu11eg
 from adidt.boards.adrv9009_pcbz import adrv9009_pcbz
--- adidt/boards/__init__.py
+++ adidt/boards/__init__.py
@@ -1,2 +1,3 @@
 from adidt.boards.daq2 import daq2
 from adidt.boards.ad9081_fmc import ad9081_fmc
+from adidt.boards.ad9081_nv_sb_s10 import ad9081_nv_sb_s10
